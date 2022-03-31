f2a734a8b0dae4058ee8cf64aa0337118223d733
Hayatullahi <hayatu4islam@yahoo.com>
2022-03-31 10:27:21 +0100

    Create Home Learning Tasks - Session 12 - R.R

---
+++ Home Learning Tasks - Session 12 - R.R
@@ -0,0 +1,32 @@
+
+first <- c(1,3,5,7,9)
+second <- c(2,4,6,8,10)
+third <- c(2,3,5,7,11)
+# Combining the three vectors to become 3x5 matrix
+merged <- c(first, second, third)
+matrics <- matrix(merged,3,5,TRUE)
+
+# printing the content of the matrix
+print(matrics)
+
+# Plot a graph and label
+plot(merged, type ="o", col="red", pch=14)
+
+# Task 2
+name <- c("Adam","Bob","Charles","David","Elijah")
+age <- c(23,45,12,32,28)
+role <- c("Manager","Accountant","Clerk","Receptionist","Secretary")
+service_length <- c(15,10,12,9,4)
+employee <- data.frame(name,age,role,service_length)
+print(employee)
+
+# Task 3
+install.packages("ggplot2")
+library(ggplot2)
+
+# to find a toy data set
+data(package = "ggplot2")
+seq <- 1:20
+
+qplot(data = mpg, x = cty, y = hwy, geom = "point", color = class)
+
